154ae7efe1a76ab8c6f9f86355a8481596726e28
Nakaaaa <nakamura.master.12106033@gmail.com>
2021-04-01 23:43:23 +0900

    参考サイトを元にものを吹き飛ばす

---
+++ Assets/Scripts/Wind.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// 参考URL：http://ftvoid.com/blog/post/742
+
+public class Wind : MonoBehaviour
+{
+    public GameObject target;
+    private Rigidbody rb;
+
+    public float coefficient;
+    public Vector3 velocity;
+
+    void Start()
+    {
+        rb = target.GetComponent<Rigidbody>();
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.CompareTag("Target"))
+        {
+            Vector3 relativeVelocity = velocity - other.attachedRigidbody.velocity;
+            other.attachedRigidbody.AddForce(coefficient * relativeVelocity);
+        }
+    }
+}
