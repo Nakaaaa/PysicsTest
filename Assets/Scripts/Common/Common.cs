032895d080c3e1f348b9fb40e602ae78e6bfb972
Nakaaaa <nakamura.master.12106033@gmail.com>
2021-04-02 00:11:56 +0900

    Common作成ならびにWind1を作成

---
+++ Assets/Scripts/Common/Common.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Common : MonoBehaviour
+{
+
+    public GameObject target;       // 衝突対象
+    protected Rigidbody targetRB;      // 衝突対象用Rigidbody
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        targetRB = target.GetComponent<Rigidbody>();
+    }
+}
---
+++ Assets/Scripts/Wind1.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+// 参考URL：http://tyju.liblo.jp/archives/5670434.html
+
+public class Wind1 : Common
+{
+    public float velocity;      // 風速
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.CompareTag("Target"))
+        {
+            Vector3 direction = new Vector3(15.0f, 0.0f, 0.0f);                                  // 方向
+            float distance = Vector3.Distance(targetRB.transform.position, direction);          // 相対距離
+            float drag = targetRB.drag;                                                         // 空気抵抗
+            targetRB.AddForce(direction * drag * velocity / distance, ForceMode.Force);
+        }
+    }
+
+}
